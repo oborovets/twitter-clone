cdc5d1024feb3c290a8c8b47c3454d047d980b59
Orest Borovets <borovets.orest@gmail.com>
2023-10-20 13:21:19 +0300

    initital migration

---
+++ sql/1u.sql
@@ -0,0 +1,36 @@
+create extension if not exists citext;
+
+create table
+  if not exists public.users (
+    id bigserial primary key,
+    username citext not null unique,
+    password text,
+    avatar text,
+    is_admin boolean default false,
+    created_at timestamp default now (),
+    updated_at timestamp default now ()
+  );
+
+create table
+  if not exists public.posts (
+    id bigserial primary key,
+    user_id bigint references public.users (id),
+    content text,
+    created_at timestamp default now (),
+    updated_at timestamp default now ()
+  );
+
+create table
+  if not exists public.follows (
+    user_id bigint not null references public.users (id),
+    follower_id bigint not null references public.users (id),
+    created_at timestamp default now (),
+    updated_at timestamp default now (),
+    unique (user_id, follower_id)
+  );
+
+create index post_user_id_index on public.posts (user_id);
+
+create index follows_user_id_index on public.follows (user_id);
+
+create index follows_follower_id_index on public.follows (follower_id);
