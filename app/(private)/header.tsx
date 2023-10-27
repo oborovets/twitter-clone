35369a71dc2a13dd4afc15176ee39491113ae50a
Orest Borovets <borovets.orest@gmail.com>
2023-10-27 18:50:14 +0300

    swr and fetcher

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,3 +1,25 @@
+"use client";
+import useSWR from "swr";
+
+const fetcher = async (url: RequestInfo | URL) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    const msg = `An error has occured while fetching: ${url}`;
+    const info = await res.json();
+    const status = res.status;
+    const error = new Error(msg);
+    console.error(info, status);
+    throw error;
+  }
+  return res.json();
+};
+
 export default function Header() {
+  const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  console.log(data);
   return <header>header</header>;
 }
