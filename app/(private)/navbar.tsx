ab4daa6b699867f66a1fab2a05c312062b3face8
Orest Borovets <borovets.orest@gmail.com>
2023-10-27 19:34:54 +0300

    highlight active link

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,20 +1,46 @@
 import Link from "next/link";
-
+import { usePathname } from "next/navigation";
 export default function NavBar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex max-w-md w-full p-5 bg-slate-800 rounded-lg my-2">
       <ul className="flex flew-row justify-around w-full">
         <li>
-          <Link href="/feed">Feed</Link>
+          <Link
+            className={pathname.startsWith("/feed") ? "text-green-400" : ""}
+            href="/feed"
+          >
+            Feed
+          </Link>
         </li>
         <li>
-          <Link href="/profile">Profile</Link>
+          <Link
+            className={pathname.startsWith("/profile") ? "text-green-400" : ""}
+            href="/profile"
+          >
+            Profile
+          </Link>
         </li>
         <li>
-          <Link href="/following">Following</Link>
+          <Link
+            className={
+              pathname.startsWith("/following") ? "text-green-400" : ""
+            }
+            href="/following"
+          >
+            Following
+          </Link>
         </li>
         <li>
-          <Link href="/followers">Followers</Link>
+          <Link
+            className={
+              pathname.startsWith("/followers") ? "text-green-400" : ""
+            }
+            href="/followers"
+          >
+            Followers
+          </Link>
         </li>
       </ul>
     </nav>
